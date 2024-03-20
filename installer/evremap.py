351a0e9a852dba7662f7164020804d609a81629f
zawkindev <zawkindev@gmail.com>
2024-03-20 23:36:18 +0500

    feat: add evremap mapping

---
+++ installer/evremap.py
@@ -0,0 +1,42 @@
+import os
+
+s = os.system
+
+evremap_toml="""
+device_name = "AT Translated Set 2 keyboard"
+
+[[dual_role]]
+input = "KEY_CAPSLOCK"
+hold = ["KEY_LEFTCTRL"]
+tap = ["KEY_CAPSLOCK"]
+"""
+
+evremap_service = """
+[Service]
+WorkingDirectory=/
+ExecStart=bash -c "/usr/bin/evremap remap /etc/evremap.toml -d 0"
+Restart=always
+
+[Install]
+WantedBy=multi-user.target
+"""
+
+s("paru -Sy evremap-git")
+
+s("touch evremap.toml")
+s("touch evremap.service")
+
+with open("evremap.toml","w") as f:
+    f.write(evremap_toml)
+
+with open("evremap.service","w") as f:
+    f.write(evremap_service)
+
+s("sudo cp evremap.service /usr/lib/systemd/system/")
+s("sudo cp evremap.toml /etc")
+s("sudo systemctl daemon-reload")
+s("sudo systemctl enable evremap.service")
+s("sudo systemctl start evremap.service")
+
+s("rm -rf evremap.service")
+s("rm -rf evremap.toml")

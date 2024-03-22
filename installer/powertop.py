c019153d8979abc76be935fc2e602f48ed840a5b
zawkindev <zawkindev@gmail.com>
2024-03-22 08:57:52 +0500

    feat: powertop

---
+++ installer/powertop.py
@@ -0,0 +1,28 @@
+import os
+
+s = os.system
+
+file_name = "powertop.service"
+
+config = '''
+[Unit]
+Description=Powertop tunings
+
+[Service]
+Type=oneshot
+RemainAfterExit=yes
+ExecStart=/usr/bin/powertop --auto-tune
+
+[Install]
+WantedBy=multi-user.target
+'''
+
+s(f"touch {file_name}")
+
+with open(file_name, "w") as f:
+    f.write(config)
+
+s(f"sudo cp {file_name} /etc/systemd/system")
+s(f"rm -rf {file_name}")
+s(f"sudo systemctl enable {file_name}")
+s(f"sudo systemctl start {file_name}")
